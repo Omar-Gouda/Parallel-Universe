279e57928fb622d3f2d17b192c6833952dd567e7
Omar Muhammad <omarg.dev1@gmail.com>
2026-02-09 14:10:57 +0000

    Updated the size of the products div on mid screen

--- src/Components/ProductsGrid.jsx
+++ src/Components/ProductsGrid.jsx
@@ -3,7 +3,7 @@ import { products } from "../Data/Products";
 
 function ProductsGrid() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-6">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         <h2 className="text-3xl font-bold text-gray-500">Welcome to Parallel Universe</h2>
         <p className="text-gray-400 mt-2">Discover a world of unique products and exclusive deals</p>
       </div>
-      <div className="text-center my-8">
+      <div className="text-center mr-12">
         <div>
         <h3 className="text-2xl font-semibold text-purple-600 mb-4">Featured Products</h3>
         </div>
